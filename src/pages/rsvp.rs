c8915d9e47173c1e12d169f35dc30c29dfdb5efe
David Kwong <20544dk@gmail.com>
2022-12-04 01:21:53 +0000

    Added basic rsvp form

--- src/pages/rsvp.rs
+++ src/pages/rsvp.rs
@@ -1,8 +1,38 @@
+mod form;
 use super::*;
+use form::*;
 
 #[function_component(RSVPPage)]
 pub fn rsvp_page() -> Html {
+    let invitee = Invitee {
+        id: "12345".to_string(),
+        fname: "David".to_string(),
+        lname: "Kwong".to_string(),
+        rsvp: Some(true),
+        dietary_requirements: "".to_string(),
+    };
     html! {
-        <div>{"RSVP PAGE"}</div>
+        <div>
+            <NavMenu<Route> routes={vec![]}/>
+            <div
+                class={"flex flex-col my-[159px] pl-[142px]"}
+            >
+                <div
+                    class={"text-[72px] mb-[56px]"}
+                >
+                    {"RSVP"}
+                </div>
+                <Form invitee={invitee}/>
+                <div class="h-[16px]"/>
+                <div>
+                    <button type="button"
+                        class="
+                        p-2 bg-black text-white w-36
+                        rounded-full
+                    "
+                    >{"SUBMIT"}</button>
+                </div>
+            </div>
+        </div>
     }
 }
---
+++ src/pages/rsvp/form.rs
@@ -0,0 +1,68 @@
+use super::*;
+
+#[derive(PartialEq, Properties)]
+pub struct FormProps {
+    pub invitee: Invitee,
+}
+
+#[function_component(Form)]
+pub fn form(props: &FormProps) -> Html {
+    {
+        let invitee = props.invitee.clone();
+        html! {
+            <div>
+                <div
+                    class="text-[36px] mb-[30px] font-semibold"
+                >
+                    {format!("{} {}", invitee.fname, invitee.lname)}
+                </div>
+                <div class="text-[28px] mb-[30px]">
+                    {"Will you be joining us?"}
+                </div>
+                <div class="mb-[50px]">
+                    <div class="mb-[21px]">
+                        <input
+                            type={"radio"}
+                            id={"coming"}
+                            name="rsvp"
+                            value={"coming"}
+                            class="invisible absolute"
+                        />
+                        <label for="coming" class="radio-label text-[18px]">
+                            {"Yes"}
+                        </label>
+                    </div>
+                    <div>
+                        <input
+                            type={"radio"}
+                            id={"not-coming"}
+                            name="rsvp"
+                            value={"not-coming"}
+                            class="invisible absolute"
+                        />
+                        <label for="not-coming" class="radio-label text-[18px]">
+                            {"No"}
+                        </label>
+                    </div>
+                </div>
+                <div class="text-[28px] mb-[19px]">
+                    {"Dietary Requirements?"}
+                </div>
+                <div>
+                    <textarea
+                        id="dietary-requirements"
+                        class="
+                            w-[400px] bg-bg h-[84px] border
+                            border-solid
+                            resize-none
+                            rounded-[2px]
+                            border-black
+                            p-4
+                        "
+                    />
+                </div>
+            </div>
+
+        }
+    }
+}
