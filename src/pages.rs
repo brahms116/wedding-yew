2e7ab7f1ddb829aa30ec39189a02e87727acd0ea
David Kwong <20544dk@gmail.com>
2022-12-13 22:12:53 +0000

    Restructure utils in pages

--- src/pages.rs
+++ src/pages.rs
@@ -2,13 +2,11 @@ use super::components::*;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
-mod auth_utils;
 mod landing;
 mod route;
 mod rsvp;
 mod rsvp_result;
 mod utils;
 
-use auth_utils::*;
 pub use route::*;
 use utils::*;
