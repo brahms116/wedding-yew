661c1844cdaeb40cedf9eeb62d251d40e86569eb
David Kwong <20544dk@gmail.com>
2022-12-25 22:55:02 +0000

    Add conditional logging only for development envs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use tracing_wasm;
 use wedding_site::App;
 
 fn main() {
-    tracing_wasm::set_as_global_default();
+    if std::env!("ENVIRONMENT") == "DEV" {
+        tracing_wasm::set_as_global_default();
+    }
     yew::Renderer::<App>::new().render();
     info!("Yew app mounted");
 }
