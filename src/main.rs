e968539138ebb8aae564bffa7b98d1f5962394e2
David Kwong <20544dk@gmail.com>
2022-12-25 22:43:13 +0000

    Enable logging

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use tracing::info;
-// use tracing_wasm;
+use tracing_wasm;
 use wedding_site::App;
 
 fn main() {
-    // tracing_wasm::set_as_global_default();
+    tracing_wasm::set_as_global_default();
     yew::Renderer::<App>::new().render();
     info!("Yew app mounted");
 }
